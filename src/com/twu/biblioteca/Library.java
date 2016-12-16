5dd52dc5bc5af5d85b4dcd7f0615c77ff2ea6bd7
fergus orbach <fo4786@gmail.com>
2016-12-16 12:42:52 +0000

    Adds library class

---
+++ src/com/twu/biblioteca/Library.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by fergyo on 16/12/2016.
+ */
+public class Library {
+
+    Library(){
+        System.out.println("Hello and welcome to the Biblioteca");
+    }
+
+}
---
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -0,0 +1,40 @@
+package com.twu.biblioteca;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by fergyo on 16/12/2016.
+ */
+public class LibraryTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+
+    @Test
+    public void welcomeMessageOnInit(){
+        new Library();
+        assertThat(outContent.toString(), CoreMatchers.containsString("Hello and welcome to the Biblioteca"));
+    }
+
+}
