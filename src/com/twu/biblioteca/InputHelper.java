3816400ac46592c36113dc94a3107f4e7b95b0ea
fergus orbach <fo4786@gmail.com>
2016-12-19 18:58:09 +0000

    adds inpput stream stubber

---
+++ src/com/twu/biblioteca/InputHelper.java
@@ -0,0 +1,23 @@
+package com.twu.biblioteca;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by fergyo on 13/12/2016.
+ */
+public class InputHelper {
+    public String getUserInput(String prompt){
+        String inputLine = null;
+        System.out.println(prompt + " ");
+        try{
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if(inputLine.length() == 0) return null;
+        }catch(IOException e){
+            System.out.println("IOException: " + e);
+        }
+        return inputLine;
+    }
+}
